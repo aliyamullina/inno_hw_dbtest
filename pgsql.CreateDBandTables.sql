8ee3fb6e2eba92019f857329d2dca873011b3134
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-29 17:36:49 +0300

    Update pgsql.CreateDBandTables.sql

--- pgsql.CreateDBandTables.sql
+++ pgsql.CreateDBandTables.sql
@@ -3,30 +3,30 @@ DROP TABLE Department, employees, customer, contract, executor;
 --CREATE DATABASE test_data;
 
 CREATE TABLE Department (
-	Id INT NOT NULL UNIQUE,
+	Id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	Name VARCHAR (255)
 );
 
 CREATE TABLE Employees (
-	id INT NOT NULL UNIQUE,
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	department_id INT,	
 	name VARCHAR (255)
 );
 
 CREATE TABLE Customer(
-	Id INT NOT NULL UNIQUE,	
+	Id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,	
   	customer_name VARCHAR(255),	
   	location VARCHAR (255)	
 );
 
 CREATE TABLE Contract (
-	id INT NOT NULL UNIQUE,
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   	customer_id INT,
-  	date DATE,
+  	date DATE NOT NULL,
   	amount INT	
 );
 
 CREATE TABLE Executor (
-	tab_no INT,
+	tab_no INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   	contract_id INT	
 );
