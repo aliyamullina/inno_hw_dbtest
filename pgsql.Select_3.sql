6c7a6ca3371d6d6ab7b0604be8cbf72ea8de7c96
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-29 20:45:20 +0300

    Update pgsql.Select_3.sql

--- pgsql.Select_3.sql
+++ pgsql.Select_3.sql
@@ -1,2 +1,9 @@
 --Найти самую часто встречающуюся локации среди всех заказчиков. 
 --Вывести: location, count
+SELECT MAX(loc_count)
+FROM (
+  SELECT location, COUNT(*) AS loc_count
+  FROM contract, customer
+  WHERE contract.customer_id = customer.id
+  GROUP BY location
+) AS FrequentLocation
