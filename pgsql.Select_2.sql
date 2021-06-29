950c25fdbf625cd5c09d444a81803f6f7eb24d33
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-29 20:09:05 +0300

    Update pgsql.Select_2.sql

--- pgsql.Select_2.sql
+++ pgsql.Select_2.sql
@@ -1,2 +1,10 @@
---Найти среднюю стоимость контрактов, заключенных сотрудников Ivan Ivanov.
+--Найти среднюю стоимость контрактов, заключенных сотрудником Ivan Ivanov.
 --Вывести: среднее значение amount
+SELECT avg(amount) as avg_amount
+FROM (
+  SELECT *
+  FROM employees, executor, contract
+  WHERE employees.employee_name = 'Ivan Ivanov'
+  AND employees.id = executor.tab_no
+  AND executor.contract_id = contract.id
+) ContractAmount
