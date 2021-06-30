130711ee40e25f38319265f9bfd7b45e3fcfd1b9
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-30 18:48:45 +0300

    Update pgsql.Select_1.sql

--- pgsql.Select_1.sql
+++ pgsql.Select_1.sql
@@ -1,11 +1,11 @@
 --Найти информацию о всех контрактах, связанных с сотрудниками департамента «Logistic». 
 --Вывести: contract_id, employee_name
-SELECT contract_id, employee_name
+SELECT logisticContracts.contract_id, logisticContracts.employee_name
 FROM (
   SELECT *
   FROM department, employees, executor
   WHERE department.department_name = 'Logistic'
   AND department.id = employees.department_id
   AND employees.id = executor.tab_no
-) t
-
+) logisticContracts
+ORDER BY logisticContracts.employee_name 
