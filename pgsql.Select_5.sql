524b7205108254eeb382632fda4cb7f560e22019
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-30 18:04:22 +0300

    Update pgsql.Select_5.sql

--- pgsql.Select_5.sql
+++ pgsql.Select_5.sql
@@ -1,2 +1,10 @@
 --Найти заказчика с наименьшей средней стоимостью контрактов. 
 --Вывести customer_name, среднее значение amount
+SELECT AVG(avgMin.amount), avgMin.customer_name
+FROM (
+  SELECT customer_name, amount
+  FROM contract, customer
+  WHERE customer.id = contract.customer_id
+) AS avgMin
+GROUP BY avgMin.customer_name
+ORDER BY AVG
