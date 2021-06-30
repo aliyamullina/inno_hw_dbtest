15bbdbd90e5b45605571acc94ef6da5dd6aafb6a
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-30 18:15:21 +0300

    Update pgsql.Select_6.sql

--- pgsql.Select_6.sql
+++ pgsql.Select_6.sql
@@ -1,2 +1,12 @@
 --Найти отдел, заключивший контрактов на наибольшую сумму. 
 --Вывести: department_name, sum
+SELECT SUM(sumMax.amount), sumMax.department_name
+FROM (
+  SELECT department_name, amount
+  FROM department, employees, executor, contract
+  WHERE department.id = employees.department_id
+  AND employees.id = executor.tab_no
+  AND contract.id = executor.contract_id
+) AS sumMax
+GROUP BY sumMax.department_name
+ORDER BY SUM DESC
