5e52747f59d024424ebc94b16d50ce569685031a
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-30 17:23:26 +0300

    Update pgsql.Select_4.sql

--- pgsql.Select_4.sql
+++ pgsql.Select_4.sql
@@ -1,2 +1,8 @@
 --Найти контракты одинаковой стоимости. 
 --Вывести count, amount
+--SELECT MAX(loc_count)
+--FROM (
+  SELECT id, amount
+  FROM contract
+  --GROUP BY amount
+--) AS FrequentLocation
