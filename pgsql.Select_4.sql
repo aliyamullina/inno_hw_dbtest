68bc67fbfccdda0c0f3ff2cd815ac214464bf403
Aliya Mullina <aliya.mullina@ya.ru>
2021-06-30 18:36:46 +0300

    Update pgsql.Select_4.sql

--- pgsql.Select_4.sql
+++ pgsql.Select_4.sql
@@ -1,9 +1,9 @@
 --Найти контракты одинаковой стоимости. 
 --Вывести count, amount
-SELECT COUNT(amount), amount
+SELECT COUNT(equalCost.amount), equalCost.amount
 FROM (
   SELECT id, amount
   FROM contract
-) AS T
-GROUP BY amount
-ORDER BY count, amount
+) AS equalCost
+GROUP BY equalCost.amount
+ORDER BY equalCost.count DESC, equalCost.amount DESC
