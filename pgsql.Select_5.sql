c18101148d7f55b203488b14d8bed7df559e3a6c
Aliya Mullina <aliya.mullina@ya.ru>
2021-07-01 13:57:22 +0300

    Update pgsql.Select_5.sql

--- pgsql.Select_5.sql
+++ pgsql.Select_5.sql
@@ -1,10 +1,28 @@
 --Найти заказчика с наименьшей средней стоимостью контрактов. 
 --Вывести customer_name, среднее значение amount
-SELECT AVG(avgMin.amount), avgMin.customer_name
+--SELECT avgMin.customer_name, AVG(avgMin.amount) as amount
+--FROM (
+  SELECT customer_name, AVG(amount)
+  FROM contract
+  JOIN customer ON customer.id = contract.customer_id
+  GROUP BY customer_name
+--) AS avgMin
+--GROUP BY avgMin.customer_name
+--ORDER BY amount
+
+/*SELECT location, localCount as Count
 FROM (
-  SELECT customer_name, amount
-  FROM contract, customer
-  WHERE customer.id = contract.customer_id
-) AS avgMin
-GROUP BY avgMin.customer_name
-ORDER BY AVG
+  	SELECT location, COUNT(*) as localCount
+ 	FROM customer
+  	GROUP BY location
+) as t_1
+JOIN (
+  SELECT MAX(localCount) AS localMax
+  FROM (
+    SELECT location, COUNT(*) as localCount
+  	FROM customer
+  	GROUP BY location
+  ) AS t_1_1
+) AS t_2
+ON t_1.localCount = t_2.localMax
+*/
