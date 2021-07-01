7344f2fc9c7626d2dbd179e21a73db8301b3e872
Aliya Mullina <aliya.mullina@ya.ru>
2021-07-01 13:32:29 +0300

    Update pgsql.Select_3.sql

--- pgsql.Select_3.sql
+++ pgsql.Select_3.sql
@@ -1,10 +1,24 @@
 --Найти самую часто встречающуюся локации среди всех заказчиков. 
 --Вывести: location, count
---SELECT MAX(frequentLocation.count)
+--SELECT max(maxLocation.count), location
 --FROM (
-  SELECT location, COUNT(*)
-  FROM contract, customer
-  WHERE contract.customer_id = customer.id
+  SELECT location, COUNT(*) as myCount
+  FROM customer
   GROUP BY location
-  ORDER BY count DESC
---) AS frequentLocation
+--) AS maxLocation
+
+select referee, OnlyMyCount
+  from (
+         select referee, count(1) as OnlyMyCount
+           from matches
+          group by referee
+       ) v_1
+  join (
+         select max(OnlyMyCount) as max_OnlyMyCount
+           from (
+                  select referee, count(1) as OnlyMyCount
+                    from matches
+                   group by referee
+                ) v_0
+       ) v_2
+    on v_1.OnlyMyCount = v_2.max_OnlyMyCount
